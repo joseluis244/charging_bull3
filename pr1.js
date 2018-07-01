b55cb9be5aefb565a92103a0c8908a038a0e56c5
joseluis244 <32981592+joseluis244@users.noreply.github.com>
2018-07-01 09:58:11 -0400

    Revert "pruebas fechas"
    This reverts commit fa18ae3494554623182cd6958c74d25a6eabdff6.

--- pr1.js
+++ pr1.js
@@ -1,19 +1,5 @@
-const cli = require("./models/clientes");
-const mongoose = require("mongoose");
-const express =  require("express");
-const app = express();
-
-app.set("view engine","ejs");
- mongoose.connect("mongodb://127.0.0.1/RB2");
-
-app.get("/",function(req,res){
-
-    cli.find({ $or: [ { "distribuye": false }, { "distribuye": false } ] },function(err,cli){
-
-        res.render("listacli.ejs",{cliente:cli})
-    })
-
-
-})
-
-app.listen(3000)
+var date = new Date();
+var date2 = date.toLocaleDateString("en-US").toString();
+date2 = date2.split("/");
+date2 = date2[2]+date2[1]+date2[0];
+console.log(date2);
